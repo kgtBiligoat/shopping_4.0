002fe12dee208c37cba88ad3702e256a3b11f847
kgtBiligoat <37581197+kgtBiligoat@users.noreply.github.com>
2018-06-19 16:43:43 +0800

    Update footerNav.vue

--- src/components/footerNav.vue
+++ src/components/footerNav.vue
@@ -4,8 +4,7 @@
 	    <div class="footer__secondary">
 	      <div class="footer__inner">
 	        <div class="footer__secondary__nav" style="position:relative;left:370px">
-	        	<span>软件体系结构大作业</span>
-	        	<span>组内成员:李麟博 邓帅 王挺宇</span>
+	        	<span>作者:李麟博</span>
 	        </div>
 	      </div>
 	    </div>
